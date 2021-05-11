649b9e57f8261e5f6ea4bf641a511791897b6500
Epoxin-Sanich <82171980+Epoxin-Sanich@users.noreply.github.com>
2021-05-12 00:18:25 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,69 @@
 $(function() {
 
-    var header = $("#header"),
+    const header = $("#header"),
         introH = $("#intro").innerHeight(),
-        scrollOffset = $(window).scrollTop();
+        scrollOffset = $(window).scrollTop;
 
-
-    /* Fixed Header */
-    checkScroll(scrollOffset);
+        checkScroll(scrollOffset);
 
     $(window).on("scroll", function() {
         scrollOffset = $(this).scrollTop();
-
+        
         checkScroll(scrollOffset);
-    });
+        });
 
     function checkScroll(scrollOffset) {
         if( scrollOffset >= introH ) {
             header.addClass("fixed");
-        } else {
+        }
+        else {
             header.removeClass("fixed");
         }
     }
 
 
-
-    /* Smooth scroll */
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
 
-        var $this = $(this),
-            blockId = $this.data('scroll'),
-            blockOffset = $(blockId).offset().top;
+        const $this = $(this),
+            blockId = $(this).data('scroll');
+            blockOfset = $(blockId).offset().top;
 
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
-
-        $("html, body").animate({
-            scrollTop:  blockOffset
-        }, 500);
+            $("#nav a").removeClass("active");
+            $this.addClass("active")
+        
+            $("html, body").animate({
+                scrollTop: blockOfset
+            },500);
     });
 
 
-
-    /* Menu nav toggle */
-    $("#nav_toggle").on("click", function(event) {
+    $("#nav_toggle, nav a").on("click", function(event) {
         event.preventDefault();
 
-        $(this).toggleClass("active");
         $("#nav").toggleClass("active");
+        $("#nav_toggle").toggleClass("active");
     });
 
 
-
-    /* Collapse */
     $("[data-collapse]").on("click", function(event) {
         event.preventDefault();
+        
+        const $this = $(this),
+            blockId = $(this).data('collapse');
 
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
+        $this.toggleClass("active")
     });
 
 
-    /* Slider */
     $("[data-slider]").slick({
         infinite: true,
         fade: false,
         slidesToShow: 1,
         slidesToScroll: 1
     });
+
+
+
 
 });
